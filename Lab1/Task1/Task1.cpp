bc12347c93a8057204515a16ad84f4472d5ab8d9
Mornigun <dets_anna@mail.ru>
2023-10-15 19:17:44 +0300

    feat: added folders task1 and task2

---
+++ Lab1/Task1/Task1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <windows.h>
+
+int main() {
+	char ch;
+	int flag = 1;
+
+	while (flag) {
+		if (GetKeyState(VK_RSHIFT) & 0x8000) {
+			std::cout << "Right Shift key pressed" << std::endl;
+			flag = 0;
+		}
+		if (GetKeyState(VK_LCONTROL) & 0x8000) {
+			std::cout << "Left Control key pressed" << std::endl;
+			flag = 0;
+		}
+	}
+
+	return 0;
+}
+
